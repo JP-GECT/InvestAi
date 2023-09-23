091f0e038da29c1919dbfd1c29b707e5be359ca4
JP-GECT <100013491+JP-GECT@users.noreply.github.com>
2023-09-24 01:43:42 +0530

    modified server

--- server.js
+++ server.js
@@ -105,60 +105,65 @@ async function sendAdvancedMessage(riskTol) {
 
 async function sendMutualFundsResponse(risk) {
   try {
- 
+   
     const response = await openai.completions.create({
       model: 'gpt-3.5-turbo-instruct',
       prompt: `The standard deviation and sortino value of certain mutual funds are provided........
-      small cap:
-      Kotak small cap Fund - standard deviation = 15.54 , sortino = 3.59
-      Quant small  cap Fund - Standard deviation = 19.27, sortino = 2.82
-      SBI small  cap Fund - Standard deviation = 13.96, sortino = 3.52
-      NIppon India small  cap Fund - Standard deviation = 16.5, sortino = 3.6
-      Axis small  cap Fund - Standard deviation = 12.83, sortino = 3.57
-      Large Cap :
-      ICICI prudential Bluechip Fund - standard deviation = 13.24 , sortino = 2.9
-      Canara Robeco Bluechip Equity Fund - standard deviation = 13 , sortino = 2.33
-      Kotak  Bluechip Fund - standard deviation = 12.96 , sortino = 2.73
-      Edelwelss Large cap Fund - standard deviation = 13.27 , sortino = 2.72
-      Axis Bluechip Fund - standard deviation = 14.75 , sortino = 1.59
+        small cap:
+        Kotak small cap Fund - standard deviation = 15.54 , sortino = 3.59,Expected return :23.91%
+        Quant small  cap Fund - Standard deviation = 19.27, sortino = 2.82 ,Expected return :30.24%
+        SBI small  cap Fund - Standard deviation = 13.96, sortino = 3.52 ,Expected return :34.04%
+        Nippon India small  cap Fund - Standard deviation = 16.5, sortino = 3.6, Expected return :24.64%
+        Axis small  cap Fund - Standard deviation = 12.83, sortino = 3.57 ,Expected return :25.31%
+        Large Cap :
+        ICICI prudential Bluechip Fund - standard deviation = 13.24 , sortino = 2.9,Expected return:14.43
+        Canara Robeco Bluechip Equity Fund - standard deviation = 13 , sortino = 2.33 ,Expected return :15.96%
+        Kotak  Bluechip Fund - standard deviation = 12.96 , sortino = 2.73,Expected return :14.74%
+        Edelwelss Large cap Fund - standard deviation = 13.27 , sortino = 2.72,Expected return :14.38%
+        Axis Bluechip Fund - standard deviation = 14.75 , sortino = 1.59,Expected return :12.62%
 
-      Mid Cap:
-      Quant Mid cap Fund - standard deviation = 17.23 , sortino = 3.21
-      PGIM India Midcap - standard deviation = 15.72 , sortino = 3.27
-      SBI Magnum Midcap Fund  - standard deviation = 14.76 , sortino = 3.33
-      Kotak Emerging Equity Fund - standard deviation = 14.39 , sortino = 3.17
-      sort the mutual funds in best to worst for an investor of risk tolerance ${risk} in the scale of 0 to 1 where 0 is less risk and 1 is high risk
-      return the reult in json format
-      return Large cap Midcap and smallcap separately
-      eg: {
-          "Investor_Risk_Tolerance": 0.8,
-          "Mutual_Funds_Ranking": [
-            {
-              "Fund_Name": "Kotak Small Cap Fund",
-              "Rank": 1
-            },
-            {
-              "Fund_Name": "Axis Small Cap Fund",
-              "Rank": 2
-            },
-            {
-              "Fund_Name": "SBI Small Cap Fund",
-              "Rank": 3
-            },
-            {
-              "Fund_Name": "Nippon India Small Cap Fund",
-              "Rank": 4
-            },
-            {
-              "Fund_Name": "Quant Small Cap Fund",
-              "Rank": 5
-            }
-          ]
-        }
-        `,
+        Mid Cap:
+        Quant Mid cap Fund - standard deviation = 17.23 , sortino = 3.21,Expected return :25.15%
+        PGIM India Midcap - standard deviation = 15.72 , sortino = 3.27 ,Expected return :23.02%
+        SBI Magnum Midcap Fund  - standard deviation = 14.76 , sortino = 3.33 ,Expected return :20.71%
+        Kotak Emerging Equity Fund - standard deviation = 14.39 , sortino = 3.17, Expected return :20.74%
+        sort the mutual funds in best to worst for an investor of risk tolerance 0.34 in the scale of 0 to 1 where 0 is less risk and 1 is high risk
+        return the result in json format
+        return Large cap Midcap and smallcap separately
+        eg: {
+            "Investor_Risk_Tolerance": 0.8,
+            "small_Mutual_Funds_Ranking": [
+              {
+                "Fund_Name": "Kotak Small Cap Fund",
+                "Rank": 1,
+                "expected return":
+              },
+              {
+                "Fund_Name": "Axis Small Cap Fund",
+                "Rank": 2,
+                "expected return":
+              },
+              {
+                "Fund_Name": "SBI Small Cap Fund",
+                "Rank": 3,
+                "expected return":
+              },
+              {
+                "Fund_Name": "Nippon India Small Cap Fund",
+                "Rank": 4,
+                "expected return":
+              },
+              {
+                "Fund_Name": "Quant Small Cap Fund",
+                "Rank": 5,
+                "expected return":
+              }
+            ]
+          }
+          `,
       
       temperature: 0, // Adjust the temperature as needed
-      max_tokens: 500, // Adjust the max_tokens based on your desired response length
+      max_tokens: 800, // Adjust the max_tokens based on your desired response length
   });
 
     return response;
@@ -218,25 +223,26 @@ app.post('/api/user-info', async(req, res) => {
 app.post('/api/user-advanced-info', async(req, res) => {
   const adData = req.body;
   const advData = adData['Survey Information']  
-  console.log(advData)
   const risk = (advData.risk + advData.options + advData.dependency + advData.loseMoney + advData.riskOptions + advData.volatility )/6;
   const calculateRisk = risk.toFixed(1);
-  console.log(calculateRisk) 
 
     try {
-      // const assistant2Response = await sendAdvancedMessage(calculateRisk);
-      // const data = JSON.parse(assistant2Response.choices[0].text);
+      const assistant2Response = await sendAdvancedMessage(calculateRisk);
+      console.log('Received form data:', assistant2Response.choices[0].text)
+      const dataValueSpliting = JSON.parse(assistant2Response.choices[0].text);
       const assistant3Response = await sendMutualFundsResponse(calculateRisk);
-      const data = JSON.parse(assistant3Response.choices[0].text)
+      console.log(assistant3Response.choices[0].text)
+      const dataFundSplit = JSON.parse(assistant3Response.choices[0].text)
+      const groupedData = {percentage:dataValueSpliting, funds:dataFundSplit}
 
 
        // Process the form data here, e.g., save it to a database
-  console.log('Received form data:', data);
+       console.log('Received form grouped', groupedData);
   
 
 
   // Respond with a success message
-  res.json(data);
+  res.json(groupedData);
     } catch (error) {
       console.log('error',error)
       // Handle errors here
